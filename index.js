9409a96d60b515ffb19f52ebd0924fff95ee1b0f
Asaf Kotzer <asafkotzer@gmail.com>
2016-02-27 16:57:58 +0200

    recursive, no email

--- index.js
+++ index.js
@@ -9,45 +9,60 @@ const sendEmail = require('./email-sender.js');
 const parseAd = require('./ad-parser.js');
 const jsonfile = require('jsonfile');
 const util = require('util')
+const blue
 const _ = require('lodash');
 
 const templateDir = path.join(__dirname, 'new-ad-email')
 const emailTemplate = new EmailTemplate(templateDir)
 
-const url = buildUrl(query.apartment);
-console.log(url)
-
 const previousAdsFilename = './previous-ads.json';
 const previousAds = (jsonfile.readFileSync(previousAdsFilename) || {}).ads || [];
 console.log(previousAds)
 
-fetch(url)
-  .then(response => response.json())
-  .then(json => {
-    const results = ((json.Private || {}).Results || []).map(x => Object.assign(x, { source: 'private' }))
-      .concat(((json.Trade || {}).Results || []).map(x => Object.assign(x, { source: 'agent' })));
+const fetchAds = url => {
+  if (!url) {
+    console.log('Loop complete')
+    return;
+  }
 
-    results
-      .filter(x => x.Type === 'Ad')
-      .map(x => parseAd(x))
-      .filter(x => !_.includes(previousAds, x.id))
-      .filter(x => x.location.latitude && x.location.longitude)
-      .filter(x => geolib.isPointInside(x.location, query.searchArea))
-      .filter(x => x.publishDate.isAfter(query.minimumPublishDate))
-      .forEach(x => {
-        previousAds.push(x.id);
-        console.log(x.id);
-        // emailTemplate.render(queryResult[0], (err, results) => {
-        //   sendEmail({ 
-        //     to: 'asafkotzer@gmail.com',
-        //     subject: results.text,
-        //     body: results.html
-        //   })
-        // })
-      });
-  })
-  .then(() => jsonfile.writeFile(previousAdsFilename, { ads: previousAds }))
-  .catch(err => console.error(err));
+  console.log('Getting page: ' + url)
+
+  fetch(url)
+    .then(response => response.json())
+    .then(json => {
+      const results = ((json.Private || {}).Results || []).map(x => Object.assign(x, { source: 'private' }))
+        .concat(((json.Trade || {}).Results || []).map(x => Object.assign(x, { source: 'agent' })));
+
+      const operations = results
+        .filter(x => x.Type === 'Ad')
+        .map(x => parseAd(x))
+        .filter(x => !_.includes(previousAds, x.id))
+        .filter(x => x.location.latitude && x.location.longitude)
+        .filter(x => geolib.isPointInside(x.location, query.searchArea))
+        .filter(x => x.publishDate.isAfter(query.minimumPublishDate))
+        .map(x => {
+          previousAds.push(x.id);
+          console.log(x.id);
+          return Promise.resolve();
+          // THIS SHOULD BE A PROMISE WE RETURN, ALONG WITH NEXTPAGEURL
+          // emailTemplate.render(queryResult[0], (err, results) => {
+          //   sendEmail({ 
+          //     to: 'asafkotzer@gmail.com',
+          //     subject: results.text,
+          //     body: results.html
+          //   })
+          // })
+        });
+
+        return Promise.all(operations)
+          .then(() => jsonfile.writeFile(previousAdsFilename, { ads: previousAds }))
+          .then(() => json.NextPageURL);
+    })
+    .then(nextPageUrl => fetchAds(nextPageUrl))
+    .catch(err => console.error(err));
+};
+
+fetchAds(buildUrl(query.apartment));
 
 console.log("waiting 10 seconds");
 setTimeout(function() { }, 10000);
