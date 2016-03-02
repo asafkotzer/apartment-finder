6572db8dc4a62179d026e36508a0a4c565c7cb37
asaf <asaf@soluto.com>
2016-03-02 17:27:14 +0200

    added image to email

--- ad-parser.js
+++ ad-parser.js
@@ -9,7 +9,7 @@ module.exports = rawJson => {
     roomCount: rawJson.Line2.split(' ')[2],
     price: (rawJson.Line3.match(/\d+/g) || []).join(''),
     publishDate: moment(rawJson.Line4, 'DD-MM-YYYY'),
-    picture: rawJson.img,
+    picture: rawJson.img.replace('/s/','/o/').replace('-mobile-s',''),
     location: {
       latitude: rawJson.latitude,
       longitude: rawJson.longitude,
