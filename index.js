1785033321ee76b8d476892ec84624898cba89fb
Asaf Kotzer <asafkotzer@gmail.com>
2016-08-20 22:54:13 +0300

    Required traits are now defined in query

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const server = require('./management/server.js')
 
 const log = message => console.log('[' + moment().format('HH:mm') + '] ' + message);
 
+Object.defineProperty(Array.prototype, 'do', { value: function(f) { for (var item of this) f(item); return this; } });
+
 const getAdsFromPage = url => {
   if (!url) {
     log('Recursion complete')
@@ -30,19 +32,27 @@ const getAdsFromPage = url => {
         .filter(x => x.location.latitude && x.location.longitude)
         .filter(x => geolib.isPointInside(x.location, query.searchArea))
         .filter(x => x.publishDate.isAfter(query.minimumPublishDate))
-        .map(x => {
-          adsRepository.updateSent(x.id);
-          return browserFilter                                          // REFACTOR PROMISE INTO MAP AND JUST 'THEN' ON RESULT
-            .checkForTraits(x.originalAdUrl, ['חניה', 'מעלית'])         // MOVE TO QUERY
-            .then(hasTraits => {
-              console.log('Does ' + x.originalAdUrl + ' have traits? ' + hasTraits);
-              if (hasTraits) {
-                log('Sending email for ad: ' + x.id);
-                return sendEmail(x);
-              }
-              return Promise.resolve(true);
-            });
-        });
+        .do(x => adsRepository.updateSent(x.id))
+        /*
+        Make checking for traits configurable (config.json)
+
+        Get the description: Array.prototype.map.call(document.querySelectorAll('.details_block_info > div > div'), el => el.innerText)
+
+        Get the pictures:
+        1. go to http://www.yad2.co.il/Nadlan/ViewImage.php?CatID=2&SubCatID={query.SubCatID}&RecordID={x.id}
+        2. on that page, execute:
+           Array.prototype.map.call(document.querySelectorAll('.imgesMuff img'), el => el.src).map(x => x.replace('/s/', '/o/').replace('-s.jpg','.jpg'))
+        3. Follow each URL to fetch the image
+        */
+        .map(x => browserFilter.checkForTraits(x.originalAdUrl, query.requiredTraits)
+          .then(hasTraits => {
+            console.log('Does ' + x.originalAdUrl + ' have traits? ' + hasTraits);
+            if (hasTraits) {
+              log('Sending email for ad: ' + x.originalAdUrl);
+              return sendEmail(x);
+            }
+            return Promise.resolve('did not send email');
+        }));
 
         return Promise.all(operations)
           .then(() => adsRepository.flush())
