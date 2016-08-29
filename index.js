a21aa1853553390b2d6a32c3b59800fcd55c4579
Asaf Kotzer <asafkotzer@gmail.com>
2016-08-29 21:00:17 +0300

    More interesting logs

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ Object.defineProperty(Array.prototype, 'do', { value: function(f) { for (var ite
 
 const checkForTraits = (ad, requiredTraits) => _.intersection(requiredTraits, ad.traits).length === requiredTraits.length;
 
-const getAdsFromPage = url => {
+const incrementCounter = (summary, counterName, addition) => summary[counterName] = (summary[counterName] + (addition || 1)) || (addition || 1);
+
+const getAdsFromPage = (url, summary) => {
   if (!url) {
-    log('Recursion complete')
+    log('Recursion complete: ' + JSON.stringify(summary))
     return;
   }
 
@@ -31,26 +33,29 @@ const getAdsFromPage = url => {
       const operations = results
         .filter(x => x.Type === 'Ad')
         .map(x => parseAd(x))
+        .do(x => incrementCounter(summary, 'retrieved', x.length))
         .filter(x => !adsRepository.wasAlreadySent(x.id))
+        .do(x => incrementCounter(summary, 'not_already_handled', x.length))
         .filter(x => x.location.latitude && x.location.longitude)
         .filter(x => geolib.isPointInside(x.location, query.searchArea))
+        .do(x => incrementCounter(summary, 'within_polygon', x.length))
         .filter(x => x.publishDate.isAfter(query.minimumPublishDate))
+        .do(x => incrementCounter(summary, 'after_min_publish_date', x.length))
         .do(x => adsRepository.updateSent(x.id))
         .map(x => {
-          console.log('pre-scraping');
           if (query.scrape) {
             return adScraper.scrape(x.originalAdUrl, x.id, x.source)
               .then(scraped => Object.assign(x, scraped))
               .then(x => {
-                console.log('Last filter:');
-                console.log(x);
                 if (checkForTraits(x, query.requiredTraits)) {
+                  incrementCounter(summary, 'passed_advanced_filter');
                   log('Sending email for ad: ' + x.originalAdUrl);
                   return sendEmail(x);
                 }
                 return Promise.resolve('did not send email');
               })
           }
+          incrementCounter(summary, 'skipped_advanced_filter');
           return sendEmail(x);
         });
 
@@ -58,13 +63,13 @@ const getAdsFromPage = url => {
           .then(() => adsRepository.flush())
           .then(() => json.NextPageURL);
     })
-    .then(nextPageUrl => getAdsFromPage(nextPageUrl))
+    .then(nextPageUrl => getAdsFromPage(nextPageUrl, summary))
     .catch(err => log(err));
 };
 
 const fetchAds = () => {
   log('Starting recursion');
-  getAdsFromPage(buildUrl(query.apartment));
+  getAdsFromPage(buildUrl(query.apartment), {});
 };
 
 server();
