9623b04e306ab77fa4aea059fbb953ebfd51d5ee
oren klein <redking12@gmail.com>
2017-07-05 02:13:32 +0300

    update email implementation

--- email-sender.js
+++ email-sender.js
@@ -1,34 +1,47 @@
 const emailConfig = require('./nconf.js').get('email');
 const path = require('path')
 const sendgrid = require('sendgrid')(emailConfig.sendgridApiKey);
+
 const EmailTemplate = require('email-templates').EmailTemplate
 const templateDir = path.join(__dirname, 'new-ad-email')
 const emailTemplate = new EmailTemplate(templateDir)
 
 const Handlebars = require('handlebars');
-Handlebars.registerHelper('formatSource', function(source) {
+Handlebars.registerHelper('formatSource', function (source) {
   return source === 'agent' ? 'תיווך' : 'פרטי';
 });
 
-const send = (options, callback) => {
-  const message = {
-    from: 'ads@apartment-finder.com',
-    fromname: 'Apartment Finder',
-    replyto: emailConfig.from,
-    to: emailConfig.to,
-    subject : options.subject,
-    html: options.body
-  };
+const helper = require('sendgrid').mail;
+const fromEmail = new helper.Email('ads@apartment-finder.com', 'Apartment Finder');
+const replyToEmail = new helper.Email(emailConfig.from);
+const personalization = new helper.Personalization();
+emailConfig.to.forEach(toEmail => {console.log(toEmail); personalization.addTo(new helper.Email(toEmail))});
 
-  sendgrid.send(message, callback);
+
+const send = (options, callback) => {
+    let mail = new helper.Mail();
+    mail.setReplyTo(replyToEmail);
+    mail.setFrom(fromEmail);
+    mail.setSubject(options.subject);
+    mail.addPersonalization(personalization);
+    const content = new helper.Content('text/html', `<html>${options.body}</html>`)
+    mail.addContent(content);
+
+    var request = sendgrid.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON()
+    });
+    sendgrid.API(request, callback);
+  
 };
 
-module.exports = model => 
+module.exports = model =>
   emailTemplate.render(model)
-    .then(results => send({ 
-      subject: results.text, 
-      body: results.html 
+    .then(results => send({
+      subject: results.text,
+      body: results.html
     },
-    (err) => {
-      if (err) console.log(err);
-    }));
+      (err) => {
+        if (err) console.log(err.response.body.errors);
+      }));
