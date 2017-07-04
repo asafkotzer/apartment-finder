932b05351ac3ae61ce0f7bf604228afaded7a1d0
oren klein <redking12@gmail.com>
2017-07-05 02:12:28 +0300

    refactor to new yad2 api

--- index.js
+++ index.js
@@ -8,34 +8,34 @@ const adsRepository = require('./ads-repository.js');
 const adScraper = require('./ad-scraper.js')
 const moment = require('moment');
 const server = require('./management/server.js')
-const _ = require('lodash');
 
 const log = message => console.log('[' + moment().format('HH:mm') + '] ' + message);
 
-Object.defineProperty(Array.prototype, 'do', { value: function(f) { for (var item of this) f(item); return this; } });
-
-const checkForTraits = (ad, requiredTraits) => _.intersection(requiredTraits, ad.traits).length === requiredTraits.length;
+Object.defineProperty(Array.prototype, 'do', { value: function (f) { for (var item of this) f(item); return this; } });
 
 const incrementCounter = (summary, counterName, addition) => summary[counterName] = (summary[counterName] + (addition || 1)) || (addition || 1);
 
 const getAdsFromPage = (url, summary) => {
-  if (!url) {
-    log('Recursion complete: ' + JSON.stringify(summary))
-    return;
-  }
 
   fetch(url)
     .then(response => response.json())
     .then(json => {
-      const results = ((json.Private || {}).Results || []).map(x => Object.assign(x, { source: 'private' }))
-        .concat(((json.Trade || {}).Results || []).map(x => Object.assign(x, { source: 'agent' })));
+           const results = (json.data || {}).feed_items || [];
 
-      const operations = results
-        .filter(x => x.Type === 'Ad')
+      let operations = results
+        .filter(x => x.type === 'ad')
         .map(x => parseAd(x))
         .do(x => incrementCounter(summary, 'retrieved', x.length))
         .filter(x => !adsRepository.wasAlreadySent(x.id))
-        .do(x => incrementCounter(summary, 'not_already_handled', x.length))
+        .do(x => incrementCounter(summary, 'not_already_handled', x.length));
+
+      if (query.appartmentTypes) {
+        operations = operations
+          .filter(x => query.appartmentTypes.includes(x.aparttype))
+          .do(x => incrementCounter(summary, 'within_apartments_types', x.length));
+      }
+
+      operations = operations
         .filter(x => x.location.latitude && x.location.longitude)
         .filter(x => geolib.isPointInside(x.location, query.searchArea))
         .do(x => incrementCounter(summary, 'within_polygon', x.length))
@@ -48,32 +48,38 @@ const getAdsFromPage = (url, summary) => {
             return adScraper.scrape(x.originalAdUrl, x.id, x.source)
               .then(scraped => Object.assign(x, scraped))
               .then(x => {
-                if (checkForTraits(x, query.requiredTraits)) {
-                  incrementCounter(summary, 'passed_advanced_filter');
-                  log('Sending email for ad: ' + x.originalAdUrl);
-                  return sendEmail(x);
-                }
-                return Promise.resolve('did not send email');
+                log('Sending email for ad: ' + x.originalAdUrl);
+                return sendEmail(x);
               })
           }
           incrementCounter(summary, 'skipped_advanced_filter');
           return sendEmail(x);
         });
 
-        return Promise.all(operations)
-          .then(() => adsRepository.flush())
-          .then(() => json.NextPageURL);
+      return Promise.all(operations)
+        .then(() => adsRepository.flush())
+        .then(() => ({ totalPages: json.data.total_pages, currentPage: json.data.current_page }));
     })
-    .then(nextPageUrl => getAdsFromPage(nextPageUrl, summary))
+    .then(({ totalPages, currentPage }) => {
+      if (totalPages == currentPage) {
+        log('Recursion complete: ' + JSON.stringify(summary))
+        return;
+      }
+      else {
+        //console.log(`another page ${currentPage + 1} from ${totalPages}`)
+        getAdsFromPage(buildUrl(query.apartment, currentPage + 1), summary)
+      }
+    })
     .catch(err => log(err));
 };
 
 const fetchAds = () => {
   log('Starting recursion');
+  //console.log(buildUrl(query.apartment));
   getAdsFromPage(buildUrl(query.apartment), {});
 };
 
 server();
 
 fetchAds();
-setInterval(() => fetchAds(), 60*60*1000);
+setInterval(() => fetchAds(), 60 * 60 * 1000);
