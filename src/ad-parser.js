2aa34556795b87bb631301adae45c0d7213f100d
Aviv Rozenboim <rubysrubys@gmail.com>
2017-07-06 00:00:31 +0300

    Parsing working

--- src/ad-parser.js
+++ src/ad-parser.js
@@ -1,23 +1,98 @@
-const moment = require('moment');
+const _ = require('lodash');
+const os = require('os');
 
-const log = message => console.log('[' + moment().format('HH:mm') + '] ' + message);
+function isNullOrWhitespace(string) {
+    return !string || string.trim().length === 0;
+}
 
-module.exports = rawJson => {
-	//log('raw jason: ' + rawJson.Line6)
-	return {
-		id: rawJson.RecordID,
-		source: rawJson.source,
-		city: rawJson.Line1.split('-')[0].trim(),
-		address: (rawJson.Line1.split('-')[1] || '').trim().replace('"', '').replace("'", ''),
-		roomCount: (rawJson.Line2.split('-')[1] || '').trim().replace('"', '').replace("'", ''),
-		aparttype: (rawJson.Line2.split('-')[0] || '').trim().replace('"', '').replace("'", ''),
-		price: (rawJson.Line3.match(/\d+/g) || []).join(''),
-		publishDate: moment(rawJson.Line4, 'DD-MM-YYYY'),
-		picture: rawJson.img.replace('/s/', '/o/').replace('-mobile-s', ''),
-		location: {
-			latitude: rawJson.latitude,
-			longitude: rawJson.longitude,
-		},
-		originalAdUrl: rawJson.URL.slice(0, rawJson.URL.indexOf('&utm')),
-	};
+function joinTextLines(...args) {
+    return _.chain(args)
+        .filter(_.negate(isNullOrWhitespace))
+        .map(str => str.replace(/\u00a0/g, ' '))
+        .join(os.EOL)
+        .value();
+}
+
+function parseInnerDetail(str) {
+    try {
+        return str.replace(/\D/g, '');
+    } catch (_) {
+        return str;
+    }
+}
+
+function parseEntrance(str) {
+    try {
+        return {
+            value: moment(str, 'DD/MM/YYYY'),
+            success: true,
+        };
+    } catch (_) {
+        return {
+            value: str,
+            success: false,
+        };
+    }
+}
+
+class Ad {
+    constructor(apiResponse) {
+        this.coordinates = _.mapValues(apiResponse.coordinates, parseFloat);
+        this.images = [apiResponse.img_url].filter(Boolean);
+        this.text = joinTextLines(apiResponse.line_1, apiResponse.line_2, apiResponse.line_3);
+        this.title = joinTextLines(apiResponse.title_1, apiResponse.title_2);
+        this.price = apiResponse.price;
+        this.id = apiResponse.id;
+        this.url = `http://yad2.co.il/s/c/${apiResponse.link_token}`;
+        this.merchant = apiResponse.merchant;
+    }
+
+    addSingleAdApi(apiResponse) {
+        const apartmentInnerDetails = _.chain(apiResponse)
+            .get('data.info_bar_items', [])
+            .keyBy('key')
+            .mapValues('title')
+            .value();
+        const parsedEntrance = parseEntrance(apartmentInnerDetails.entrance);
+        this.entrance = parsedEntrance.value;
+        this.isInstantEntrance = !parsedEntrance.success;
+        this.floor = parseInnerDetail(apartmentInnerDetails.floor);
+        this.rooms = parseInnerDetail(apartmentInnerDetails.rooms);
+        this.meter = parseInnerDetail(apartmentInnerDetails.meter);
+
+        this.extraInfo = _.chain(apiResponse)
+            .get('data.additional_info', {})
+            .values()
+            .filter(_.negate(isNullOrWhitespace))
+            .join(os.EOL)
+            .value();
+        this.traits = _.chain(apiResponse)
+            .get('data.additional_info_items_v2', [])
+            .keyBy('key')
+            .mapValues('value')
+            .value();
+        this.extraData = _.chain(apiResponse)
+            .get('data.important_info_items', [])
+            .keyBy('key')
+            .mapValues('value')
+            .value();
+        this.images = _.chain(apiResponse)
+            .get('data.images', [])
+            .concat(this.images)
+            .compact()
+            .map(str => str.trim())
+            .uniq()
+            .value();
+        this.adNumber = _.get(apiResponse, 'data.ad_number', 'unknown');
+        this.url = _.get(apiResponse, 'data.canonical_url');
+    }
+}
+
+function parseAds(apiResponse) {
+    return apiResponse.data.feed_items.filter(item => item.id).map(item => new Ad(item));
+}
+
+module.exports = {
+    parseAds,
+    Ad,
 };
---
+++ src/fetcher.js
@@ -0,0 +1,32 @@
+const _ = require('lodash');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+const query = require('../config/query');
+
+const BASE_URL = 'https://app.yad2.co.il/api/v1.0/';
+const BASE_PAGE_URL = BASE_URL + 'feed/feed.php';
+const BASE_AD_URL = BASE_URL + 'ad/ad.php';
+
+function fetchPage(options) {
+	const queryParams = Object.assign({}, query.apartment, options);
+	const formData = new FormData();
+
+	Object.keys(queryParams).forEach(key => {
+		formData.append(key, query.apartment[key]);
+	});
+
+	return fetch(BASE_PAGE_URL, {
+		method: 'post',
+		body: formData,
+	}).then(r => r.json());
+}
+
+function fetchAd(ad) {
+	const adUrl = BASE_AD_URL + '?id=' + ad.id;
+	return fetch(adUrl).then(r => r.json());
+}
+
+module.exports = {
+	fetchPage,
+	fetchAd,
+};
